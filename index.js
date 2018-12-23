c6962076af7ff0d2b7c32a5770193a6ba07e4e66
hen aharon <henaharon@se.shenkar.ac.il>
2018-12-23 19:49:50 +0200

    release 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan');
+//const morgan = require('morgan');
 const ctrl = require('./controller');
 
 const app = express();
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 app.get('/game', ctrl.getAllGames);
 app.get('/game/:type&:date', ctrl.getGameByTypeDate);
