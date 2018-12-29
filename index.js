44a96ca2c04a3d7517d6ef074a660dc8805d28a2
hen aharon <henaharon@se.shenkar.ac.il>
2018-12-29 16:06:58 +0200

    LastSub

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const ctrl = require('./controller');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(morgan('dev'));
+
+
+
+app.get('/api', (req,res) => {
+    res.redirect("https://documenter.getpostman.com/view/5677398/Rzn6v2mb")
+});
 
 app.get('/game', ctrl.getAllGames);
 app.get('/game/:type&:date', ctrl.getGameByTypeDate);
 app.post('/game/:id', ctrl.editGame);
+
+app.all('*',(req,res,next) => {
+    console.log("Hello client for details enter couplesgames1.herokuapp.com/api ")
+});
 
 app.listen(port, 
     () => console.log(`listening on port ${port}`)
