8ba4a9128f5fe04c5117dc1f0c6508390d4e2c2c
hen aharon <henaharon@se.shenkar.ac.il>
2018-12-29 16:28:11 +0200

    Last

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
-
+app.all('*',(req,res,next) => {
+    console.log("Hello client for details enter couplesgames1.herokuapp.com/api ")
+    next();
+});
 
 app.get('/api', (req,res) => {
     res.redirect("https://documenter.getpostman.com/view/5677398/Rzn6v2mb")
@@ -19,9 +22,7 @@ app.get('/game', ctrl.getAllGames);
 app.get('/game/:type&:date', ctrl.getGameByTypeDate);
 app.post('/game/:id', ctrl.editGame);
 
-app.all('*',(req,res,next) => {
-    console.log("Hello client for details enter couplesgames1.herokuapp.com/api ")
-});
+
 
 app.listen(port, 
     () => console.log(`listening on port ${port}`)
